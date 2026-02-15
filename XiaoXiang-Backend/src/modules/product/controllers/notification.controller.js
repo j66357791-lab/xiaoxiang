e7cb02734ec1a60531be5ff3d651ad107d8139fe
j66357791-lab <j66357791@gmail.com>
2026-02-15 21:58:23 +0800

    Delete XiaoXiang-Backend/src directory

--- XiaoXiang-Backend/src/modules/product/controllers/notification.controller.js
+++
@@ -1,9 +0,0 @@
-import * as service from '../services/notification.service.js';
-import { success } from '../../../common/utils/response.js';
-import { asyncHandler } from '../../../common/utils/asyncHandler.js';
-
-export const registerToken = asyncHandler(async (req, res) => {
-  const { token, platform } = req.body;
-  await service.savePushToken(req.user._id, token, platform);
-  return success(res, '注册成功');
-});
--- XiaoXiang-Backend/src/modules/product/routes/notification.routes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { authenticate } from '../../../common/middlewares/auth.js';
-import * as ctrl from '../controllers/notification.controller.js';
-const router = express.Router();
-
-router.post('/register', authenticate, ctrl.registerToken);
-export default router;
